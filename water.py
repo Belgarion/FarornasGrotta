3cb38bdeea14a022e426247c3f618d78dc5e911d
Sebastian <sebbe1991+git@gmail.com>
2009-12-10 20:01:26 +0100

    Water, Fixed calculation of normals.

--- water.py
+++ water.py
@@ -28,10 +28,11 @@ class Water:
 
 		vec3 computeNormal(vec3 pos, vec3 tangent, vec3 binormal) {
 			mat3 J;
-			float y = pos.y + sin(0.2 * pos.x + time) * cos(0.2 * pos.z + time) * 2.0;
+			float yx = 0.4 * cos(0.2 * pos.x + time);
+			float yz = -0.4 * sin(0.2 * pos.z + time);
 
 			J[0][0] = 1.0;
-			J[0][1] = y * pos.x;
+			J[0][1] = yx;
 			J[0][2] = 0.0;
 
 			J[1][0] = 0.0;
@@ -39,7 +40,7 @@ class Water:
 			J[1][2] = 0.0;
 
 			J[2][0] = 0.0;
-			J[2][1] = y * pos.z;
+			J[2][1] = yz;
 			J[2][2] = 1.0;
 
 			vec3 u = J * tangent;
@@ -55,7 +56,7 @@ class Water:
 
 			// Compute vertex position
 			vec4 a = gl_Vertex;
-			a.y = a.y + sin(0.2 * a.x + time) * cos(0.2 * a.z + time) * 2.0;
+			a.y = a.y + (sin(0.2 * a.x + time) + cos(0.2 * a.z + time)) * 2.0;
 			gl_Position = gl_ModelViewProjectionMatrix * a;
 			///
 
@@ -78,6 +79,8 @@ class Water:
 			binormal = normalize(binormal);
 
 			newNormal = computeNormal(a.xyz, tangent.xyz, binormal);
+			newNormal = -newNormal;
+
 			vNormal = normalize(gl_NormalMatrix * newNormal);
 
 			normal = gl_NormalMatrix * newNormal.xyz;
@@ -88,6 +91,8 @@ class Water:
 			eyeVec = -ecPos;
 		}
 		"""]
+
+
 		fragmentSource = ["""
 		varying vec3 normal, lightDir, eyeVec;
 		
@@ -115,6 +120,8 @@ class Water:
 								specular;
 			}
 			gl_FragColor = final_color;
+
+			//gl_FragColor = float4( (normal + 1) / 2.0, 1);
 		}
 		"""]
 		glShaderSourceARB(vertexShader, vertexSource)
